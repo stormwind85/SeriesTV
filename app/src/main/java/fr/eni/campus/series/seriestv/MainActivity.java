ea87fad7fb483e9e4fb0708c2383b7d02ea71e0d
stormwind85 <adam.brechet@gmail.com>
2018-06-26 17:29:22 +0200

    Display image serie on list & show text informations about serie

--- app/src/main/java/fr/eni/campus/series/seriestv/MainActivity.java
+++ app/src/main/java/fr/eni/campus/series/seriestv/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String API_TOKEN = "Bearer f17e68d82c20";
     private static final int MAX_ITEMS_PER_REQUEST = 3;
     private static final int SIMULATED_LOADING_TIME_IN_MS = 1500;
-    private static int TOTAL_LIMIT_SERIES = 10;
+    private static int TOTAL_LIMIT_SERIES = 9;
 
     private DrawerLayout drawerLayout;
     private Toolbar toolbar;
@@ -123,7 +123,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void startRequestToAPI() {
-        String url = ENDPOINT + "/shows/list?limit=" + TOTAL_LIMIT_SERIES;
+        String url = ENDPOINT + "/shows/list?order=followers&limit=" + TOTAL_LIMIT_SERIES;
         series = new LinkedList<>();
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
             (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
@@ -140,7 +140,7 @@ public class MainActivity extends AppCompatActivity {
                                         shows.getJSONObject(i).getLong("id"),
                                         shows.getJSONObject(i).getString("title"),
                                         shows.getJSONObject(i).getString("status"),
-                                        shows.getJSONObject(i).getJSONObject("images").getString("show"),
+                                        shows.getJSONObject(i).getJSONObject("images").getString("box"),
                                         shows.getJSONObject(i).getInt("creation"), new LinkedList<Saison>());
 
                                 JSONArray saison_details = shows.getJSONObject(i).getJSONArray("seasons_details");
--- app/src/main/java/fr/eni/campus/series/seriestv/SerieAdapter.java
+++ app/src/main/java/fr/eni/campus/series/seriestv/SerieAdapter.java
@@ -1,11 +1,15 @@
 package fr.eni.campus.series.seriestv;
 
+import android.content.Context;
+import android.content.res.Configuration;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -13,6 +17,10 @@ import fr.eni.campus.series.seriestv.model.Serie;
 
 class SerieAdapter extends RecyclerView.Adapter<SerieAdapter.SerieHolder> {
     private List<Serie> series;
+    private Context context;
+    private float density;
+    private int widthParent;
+    private int heightParent;
 
     public SerieAdapter(List<Serie> series) {
         this.series = series;
@@ -20,15 +28,28 @@ class SerieAdapter extends RecyclerView.Adapter<SerieAdapter.SerieHolder> {
 
     @Override
     public SerieHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        context = parent.getContext();
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_serie, parent, false);
         SerieHolder serieHolder = new SerieHolder(view);
+
+        density = parent.getResources().getDisplayMetrics().density;
+        widthParent = parent.getWidth();
+        if(parent.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)
+            heightParent = (int)(150 * density + 0.5f);
+        else
+            heightParent = (int)(100 * density + 0.5f);
+
         return serieHolder;
     }
 
     @Override
     public void onBindViewHolder(SerieHolder holder, int position) {
         Serie currentSerie = series.get(position);
-        holder.text.setText(currentSerie.toString());
+        holder.imageView.getLayoutParams().width = widthParent - 50;
+        holder.imageView.getLayoutParams().height = heightParent;
+        holder.imageView.requestLayout();
+        Picasso.with(context).load(currentSerie.getImageUrl()).fit().centerInside().into(holder.imageView);
+        holder.textView.setText(currentSerie.getTitle() + " - status " + currentSerie.getStatus() + " - " + currentSerie.getSaisons().size() + " saisons");
     }
 
     @Override
@@ -42,10 +63,13 @@ class SerieAdapter extends RecyclerView.Adapter<SerieAdapter.SerieHolder> {
 
     public class SerieHolder extends RecyclerView.ViewHolder {
         protected TextView text;
+        protected ImageView imageView;
+        protected TextView textView;
 
         public SerieHolder(View itemView) {
             super(itemView);
-            text = itemView.findViewById(R.id.item_text);
+            imageView = itemView.findViewById(R.id.img_serie);
+            textView = itemView.findViewById(R.id.info_serie);
         }
     }
 }

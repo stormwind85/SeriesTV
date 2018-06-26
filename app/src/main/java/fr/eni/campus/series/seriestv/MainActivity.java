89ead66d7644fe7d73ffa0317b12162db6313781
DESKTOP-A02FR2F\bapti <baptiste.rivalin@gmail.com>
2018-06-26 11:00:10 +0200

    Bar de recherche

--- app/src/main/java/fr/eni/campus/series/seriestv/MainActivity.java
+++ app/src/main/java/fr/eni/campus/series/seriestv/MainActivity.java
@@ -1,12 +1,16 @@
 package fr.eni.campus.series.seriestv;
 
+import android.app.SearchManager;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         enableLeftMenu();
+
     }
 
     @Override
@@ -29,6 +34,16 @@ public class MainActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_menu, menu);
+        // Retrieve the SearchView and plug it into SearchManager
+        final SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        return true;
     }
 
     private void enableLeftMenu(){
